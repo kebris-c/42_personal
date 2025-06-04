2be8306d812f637558f766b53017b84454b868db
Kevin Bris <55791683+kebris-c@users.noreply.github.com>
2025-06-04 15:18:52 +0200

    Update get_next_line_bonus.h

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -13,34 +13,21 @@
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
-//	defines
+// Headers
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+// Buffer size definition
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
-//	headers
-# include <stdio.h>
-# include <unistd.h>
-# include <fcntl.h>
-# include <stddef.h>
-# include <stdlib.h>
-
-//	typedefs
-typedef struct s_line
-{
-	int				fd;
-	char			*stash;
-	char			*line;
-	char			*newline_ptr;
-	struct s_line	*curr;
-	struct s_line	*next;
-}	t_line;
-
-//	prototypes
-char	*get_next_line(int fd);
-void	ft_init_node(t_line *node, int fd);
-void	ft_del_node(t_line **fd_line, int fd);
-char	*ft_strjoin_and_free(char *s1, char *s2);
-char	*ft_strchr(char *stash, char nl);
+// Function prototypes
+char    *get_next_line(int fd);
+char    *ft_strchr(const char *s, int c);
+char    *ft_strjoin(const char *s1, const char *s2);
+char    *ft_strdup(const char *s);
+size_t  ft_strlen(const char *s);
 
 #endif
